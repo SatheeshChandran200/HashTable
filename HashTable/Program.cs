488a8d8fc7c5a62586c594b12958ecdc01f0e921
Satheesh Chandran <chandransatheesh200@hotmail.com>
2017-09-07 17:00:00 +0530

    Second commit

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -10,8 +10,11 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
+            Module1 myObj = new Module1();
+            myObj.Name = "AppAtlas";
             Console.WriteLine("This is first GitHub test");
             Console.WriteLine("This is GitHub test for changes");
+            Console.WriteLine(myObj.Name);
             Console.ReadLine();
         }
     }
